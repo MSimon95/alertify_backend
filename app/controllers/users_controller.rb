a09c06206afaf8ad5b2b8bd0ac0c5ac3d52a60a5
github username <github email address>
2021-06-13 15:13:54 -0400

    destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,13 +28,13 @@ class UsersController < ApplicationController
     def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@users)
+
     end
     
     def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to users_path
+  
     end
     
     
